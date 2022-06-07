ff8132afe1b626df253f6edf75c4ad503f48fc00
Christopher Marie-Angelique <christopher2211.angelique@gmail.com>
2022-06-07 14:53:26 +0200

    Ajout fonctionnalité générer PDF

--- DailyBank workspace/DAILY_BANK_FX_V0/src/application/view/OperationsManagementController.java
+++ DailyBank workspace/DAILY_BANK_FX_V0/src/application/view/OperationsManagementController.java
@@ -1,9 +1,23 @@
 package application.view;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;	
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.ResourceBundle;
+
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import application.DailyBankState;
 import application.control.OperationsManagement;
@@ -59,7 +73,7 @@ public class OperationsManagementController implements Initializable {
 	public void displayDialog() {
 		this.primaryStage.showAndWait();
 	}
-	
+
 
 	// Gestion du stage
 	private Object closeWindow(WindowEvent e) {
@@ -112,7 +126,7 @@ public class OperationsManagementController implements Initializable {
 			this.validateComponentState();
 		}
 	}
-	
+
 	@FXML
 	private void doVirement(){
 		Operation op = this.om.enregistrerVirement();
@@ -125,16 +139,109 @@ public class OperationsManagementController implements Initializable {
 	@FXML
 	private void doAutre() {
 	}
-	
+
 	@FXML
 	private void doGenererPdf() {
-		System.out.println("pdf");
+		int selectedIndice = this.lvOperations.getSelectionModel().getSelectedIndex();
+		Operation op = this.lvOperations.getSelectionModel().getSelectedItem();
+
+
+		Document doc = new Document();
+		try {
+			PdfWriter.getInstance(doc, new FileOutputStream("relevé-bancaire-mensuel.pdf"));
+			doc.open();
+
+			Font titleFont = FontFactory.getFont(FontFactory.TIMES,40f);
+			Font subTitleFont = FontFactory.getFont(FontFactory.TIMES,20f);
+			Font contentFont = FontFactory.getFont(FontFactory.TIMES,14f);
+
+			String infoCli = "Nom du client : " + this.clientDuCompte.nom + 
+					"\nPrénom du client : " + this.clientDuCompte.prenom +
+					"\nAdresse postal du client : " + this.clientDuCompte.adressePostale +
+					"\nNuméro de téléphone du client : " + this.clientDuCompte.telephone +
+					"\nMail du client : " + this.clientDuCompte.email;
+
+			String infoCompte = "Numéro du compte : " + this.compteConcerne.idNumCompte +
+					"\nDécouvert autorisé : " + this.compteConcerne.debitAutorise +
+					"\nSolde du compte : " + this.compteConcerne.solde;
+			String infoOperation = "";
+
+
+			//Crée des paragraphes sur le doc pdf
+			Paragraph para = new Paragraph("Relevé bancaire : DailyBank", titleFont);
+			Paragraph ligne = new Paragraph("______________________", titleFont);
+			Paragraph content = new Paragraph(infoCli, contentFont);
+			Paragraph content2 = new Paragraph(infoCompte, contentFont);
+
+			// Aligner les paragraphes au centre
+			ligne.setAlignment(Element.ALIGN_CENTER);
+			para.setAlignment(Element.ALIGN_CENTER);
+
+
+			para.setFont(titleFont);
+			content.setFont(contentFont);
+
+			doc.add(para);
+			doc.add(ligne);
+			doc.add(Chunk.NEWLINE);
+			doc.add(new Paragraph("Informations sur le client :", subTitleFont));
+			doc.add(Chunk.NEWLINE);
+			doc.add(content);
+			doc.add(Chunk.NEWLINE);
+			doc.add(new Paragraph("Informations sur le compte bancaire :", subTitleFont));
+			doc.add(Chunk.NEWLINE);
+			doc.add(content2);
+			doc.add(Chunk.NEWLINE);
+			doc.add(new Paragraph("Informations sur les opération du compte :", subTitleFont));
+			doc.add(Chunk.NEWLINE);
+			// On récupère la liste des opération du compte
+			for(int i = 0; i < this.olOperation.size(); i++) {
+				System.out.println(this.olOperation.get(i));
+				String leleu = this.olOperation.get(i).toString();
+				Paragraph content3 = new Paragraph(leleu, contentFont);
+				doc.add(content3);
+			}
+
+
+
+			doc.close();
+
+			try {
+				Desktop.getDesktop().open(new File("Relevé-bancaire-mensuel.pdf"));
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(DocumentException d) {
+			d.printStackTrace();
+		}
+
+		/*
+		 String pdf = "bilanSAE2.pdf";
+
+		Document document = new Document(PageSize.A4);
+
+		PdfWriter.getInstance(document, new FileOutputStream(pdf));
+
+		document.open();
+
+		document.add(new Phrase("hello world"));
+
+		document.close();
+		System.out.println("Document : " + pdf + " generated");
+
+		System.out.println("voulez-vous générer un PDF ?");
+		 */
 	}
+
 
 	private void validateComponentState() {
 		this.btnCredit.setDisable(false);
-			this.btnDebit.setDisable(false);
-			this.btnVirement.setDisable(false);
+		this.btnDebit.setDisable(false);
+		this.btnVirement.setDisable(false);
 		if(this.dbs.isChefDAgence()) {
 			this.btnGenererPdf.setDisable(false);
 		}else {
